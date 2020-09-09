ac9fe12ec476ac18f7e8e22036be1e3ff92a924f
Sobcat <771954909@qq.com>
2020-09-09 16:11:43 +0800

    now

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,64 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/"> {{title}} </router-link> |
       <router-link to="/about">About</router-link>
+      <span v-html="html_m"></span>
+      <a v-bind:href="url">点我链接</a>
+      <input type="text" v-model="msg">
+      <div> {{msg}} - {{reversedMsg}} </div>
+      <h1 v-if='bool'>1</h1>
+      <h1 v-else>2</h1>
+      <button v-on:click="ab">切换</button>
+      <div>
+        <button @click="but1">but1</button>
+        <button @click="but2(msg)">but2</button>
+        <button @click="but3(msg,$event)">but3</button>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+// import func from '../vue-temp/vue-editor-bridge';
+  export default {
+    data() {
+      return {
+        title: 'Home',
+        html_m: "<h1>是我</h1>",
+        url: "http://www.baidu.com",
+        msg: 'fuck',
+        bool: false
+      }
+    },
+    computed: {
+      reversedMsg: function(){
+        return this.msg.split('').reverse().join('');
+      }
+    },
+    methods: {
+      // reversedMsg: function(){
+      //   return this.msg.split('').reverse().join('');
+      // }
+      ab: function(){
+        this.bool = !this.bool;
+      },
+      but1(event){              
+        alert(event.target.innerText);
+      },
+      but2(msg){
+        alert(msg);
+      },
+      but3(msg,$event){
+        alert(msg+$event.target.innerText);
+      }
+    },
+    watch: {
+
+    }
+  }
+</script>
 
 <style lang="less">
 #app {
